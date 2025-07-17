ea5544a395dd86d9914799cce103c68ad601eafa
abyssFrancesco <gragnaniellofrancescom@gmail.com>
2025-07-17 11:51:04 +0200

    ssaa

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCards = () => {
   return (
-    <div className='titlecards'>TitleCards</div>
+    <div className='titlecards'>
+      <h2>Popular on Netflix</h2>
+    </div>
   )
 }
 export default TitleCards;
