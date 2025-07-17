2693fdf2f810a5192cf5f4ce4b1b736373f74bb0
abyssFrancesco <gragnaniellofrancescom@gmail.com>
2025-07-17 20:32:49 +0200

    adsdsa

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCards = () => {
   return (
-    <div className='titlecards'>
+    <div className="titlecards">
       <h2>Popular on Netflix</h2>
+      <div className="card-list">
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="" />
+            </div>
+          );
+        })}
+        ;
+      </div>
     </div>
-  )
-}
+  );
+};
 export default TitleCards;
